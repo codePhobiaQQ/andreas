407b48cedbe01ab70967205066b91e16109b8e72
helloworld <vital.pereg@mail.ru>
2021-07-15 13:33:20 +0300

    to mackbook

--- client/components/auth.tsx
+++ client/components/auth.tsx
@@ -1,21 +1,34 @@
-import React, {Dispatch} from 'react'
-import {Nav, Tab} from 'react-bootstrap';
+import React, {Dispatch, useState} from 'react'
+import {Nav, Spinner, Tab} from 'react-bootstrap';
 import close from "../assets/svg/close.svg";
 import authImg from "../assets/img/auth-img.png"
 import axios from "axios"
+import RegistrationTab from "./authTabs/RegistrationTab";
 
 interface IAuth {
   setAuthVisible: Dispatch<React.SetStateAction<boolean>>
 }
 
+interface IRegistrationTab {
+  email: string
+  password: string
+  name: string
+}
+
 const Auth = (props: IAuth) => {
 
-  const registration = async () => {
+  let [loading, setLoading] = useState(false);
+
+  const registration = async (data: IRegistrationTab) => {
     try {
-      const user = await axios.post('http://localhost:5000/auth/registration', {name: "test", email: "testingi@mail.ru", password: "12345678"})
+      setLoading(true)
+      const user = await axios.post('http://localhost:5000/auth/registration', {name: data.name, email: data.email, password: data.password})
+      setTimeout(() => setLoading(false), 500)
       console.log(user)
+      return user
     } catch (e) {
       console.log(e.messge)
+      setTimeout(() => setLoading(false), 500)
     }
   }
 
@@ -29,21 +42,23 @@ const Auth = (props: IAuth) => {
           Write us whatever you want. We will answer to you as fast as possible.
         </p>
         <Tab.Container id="left-tabs-example" defaultActiveKey="SingIn">
-          <Nav variant="pills">
-            <Nav.Item>
-              <Nav.Link eventKey="SingIn">Sing in</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="Register">Register</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="Test">Test</Nav.Link>
-            </Nav.Item>
-          </Nav>
+
+          {loading
+            ? <Spinner animation="grow"  variant="primary" />
+              : <Nav variant="pills">
+              <Nav.Item>
+                <Nav.Link eventKey="SingIn">Sing in</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="Register">Register</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="Test">Test</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          }
 
           <Tab.Content>
-
-
             <Tab.Pane eventKey="SingIn">
               <label htmlFor="email">Email</label>
               <input id="email" type="text"/>
@@ -56,7 +71,9 @@ const Auth = (props: IAuth) => {
 
 
             <Tab.Pane eventKey="Register">
-              ahahah2
+              <RegistrationTab
+                registration={registration}
+              />
             </Tab.Pane>
             <Tab.Pane eventKey="Test">
               ahahah3
---
+++ client/components/authTabs/RegistrationTab.tsx
@@ -0,0 +1,31 @@
+import { AxiosResponse } from 'axios';
+import React, {useState} from 'react'
+import {Tab} from "react-bootstrap";
+
+interface IRegistration {
+  registration: any
+}
+
+const RegistrationTab = (props: IRegistration) => {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  return (
+    <>
+      <label htmlFor="Reg-name">Name</label>
+      <input onChange={(e) => setName(e.target.value)} id="Reg-name" type="text"/>
+
+      <label htmlFor="Reg-email">Email</label>
+      <input onChange={(e) => setEmail(e.target.value)} id="Reg-email" type="text"/>
+
+      <label htmlFor="Reg-password">Password</label>
+      <input onChange={(e) => setPassword(e.target.value)} id="Reg-password" type="text"/>
+
+      <button onClick={() => props.registration({name, email, password})}>Start the test</button>
+    </>
+  );
+};
+
+export default RegistrationTab;
