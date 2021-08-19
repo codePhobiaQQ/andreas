bf5cae3d96ebfe5ce04ba229dbe45597582f489a
vitalik-fan <63237530+vitalik-fan@users.noreply.github.com>
2021-08-19 15:01:53 +0300

    Перед тем как нормально настроить миграции

--- server/dist/middleware/response.middleware.js
+++ server/dist/middleware/response.middleware.js
@@ -10,7 +10,7 @@ exports.ResponseMiddleware = void 0;
 const common_1 = require("@nestjs/common");
 let ResponseMiddleware = class ResponseMiddleware {
     use(request, response, next) {
-        response.header('Access-Control-Allow-Origin', 'https://testing.nikitin-sergei.ru');
+        response.header('Access-Control-Allow-Origin', 'http://localhost:3000');
         response.header('Access-Control-Allow-Credentials', 'true');
         response.header('Access-Control-Allow-Methods', '*');
         next();
--- server/src/middleware/response.middleware.ts
+++ server/src/middleware/response.middleware.ts
@@ -4,10 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class ResponseMiddleware implements NestMiddleware {
   use(request: Request, response: Response, next: NextFunction) {
-    response.header(
-      'Access-Control-Allow-Origin',
-      'https://testing.nikitin-sergei.ru',
-    );
+    response.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     response.header('Access-Control-Allow-Credentials', 'true');
     response.header('Access-Control-Allow-Methods', '*');
     next();
